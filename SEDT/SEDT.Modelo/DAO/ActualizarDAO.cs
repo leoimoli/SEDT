4a88b60088f23cd7fb046e2fc98981ba88616c99
leandroroldan <leodimonio@gmail.com>
2017-07-24 16:20:14 -0300

    agregado de script updates

--- SEDT/SEDT.Modelo/DAO/ActualizarDAO.cs
+++ SEDT/SEDT.Modelo/DAO/ActualizarDAO.cs
@@ -14,11 +14,11 @@ namespace SEDT.Modelo.DAO
     public class ActualizarDAO
     {
         public static MySql.Data.MySqlClient.MySqlConnection connection = new MySqlConnection(ConfigurationManager.AppSettings.Get("DB"));
-        public static int AltaEquipoUsuario(EquipoUsuario equipo)
+        public static int ActualizarEquipoUsuario(EquipoUsuario equipo)
         {
             int id = 1;
             connection.Open();
-            string proceso = "AltaEquipoUsuario";
+            string proceso = "ActualizarEquipoUsuario";
             MySqlCommand cmd = new MySqlCommand(proceso, connection);
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.Parameters.AddWithValue("Nombre_in", equipo.NombreEquipo);
@@ -27,16 +27,17 @@ namespace SEDT.Modelo.DAO
             cmd.Parameters.AddWithValue("Siglas_in", equipo.Siglas);
             cmd.Parameters.AddWithValue("SitioWeb_in", equipo.SitioWeb);
             cmd.Parameters.AddWithValue("TelefonoDeContacto_in", equipo.TelefonoDeContacto);
+            cmd.Parameters.AddWithValue("Id", equipo.IdEquipoUsuario);
             cmd.ExecuteNonQuery();
             connection.Close();
             return id;
         }
-        public static int AltaPersonaFisicaJugador(PersonaFisicaJugador jugador)
+        public static int ActualizarPersonaFisicaJugador(PersonaFisicaJugador jugador)
         {
             int id = 1;
             connection.Open();
             ///PROCEDIMIENTO
-            string proceso = "AltaPersonaFisicaJugador";
+            string proceso = "ActualizarPersonaFisicaJugador";
             MySqlCommand cmd = new MySqlCommand(proceso, connection);
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.Parameters.AddWithValue("txDni_in", jugador.Dni);
@@ -48,15 +49,16 @@ namespace SEDT.Modelo.DAO
             cmd.Parameters.AddWithValue("txPeso_in", jugador.Peso);
             cmd.Parameters.AddWithValue("imagen_in", jugador.Imagen);
             cmd.Parameters.AddWithValue("idUsuario_in", jugador.idUsuario);
+            cmd.Parameters.AddWithValue("Id", jugador.IdPersonaFisicaJugador);
             cmd.ExecuteNonQuery();
             connection.Close();
             return id;
         }
-        public static int AltaFichaTecnicaJugador(FichaTecnicaJugador ficha)
+        public static int ActualizarFichaTecnicaJugador(FichaTecnicaJugador ficha)
         {
             int id = 1;
             connection.Open();
-            string proceso = "AltaFichaTecnicaJugador";
+            string proceso = "ActualizarFichaTecnicaJugador";
             MySqlCommand cmd = new MySqlCommand(proceso, connection);
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.Parameters.AddWithValue("PosicionDeCampo_in", ficha.PosicionDeCampo);
@@ -72,34 +74,37 @@ namespace SEDT.Modelo.DAO
             cmd.Parameters.AddWithValue("TecnicaIndividual_in", ficha.TecnicaIndividual);
             cmd.Parameters.AddWithValue("DisciplinaTactica_in", ficha.DisciplinaTactica);
             cmd.Parameters.AddWithValue("IdPersonaFisicaJugador_in", ficha.IdPersonaFisicaJugador);
+            cmd.Parameters.AddWithValue("Id", ficha.IdFichaTecnicaJugador);
             cmd.ExecuteNonQuery();
             connection.Close();
             return id;
         }
-        public static int AltaTorneo(Torneo torneo)
+        public static int ActualizarTorneo(Torneo torneo)
         {
             int id = 1;
             connection.Open();
-            string proceso = "AltaTorneo";
+            string proceso = "ActualizarTorneo";
             MySqlCommand cmd = new MySqlCommand(proceso, connection);
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.Parameters.AddWithValue("NombreTorneo_in", torneo.NombreTorneo);
             cmd.Parameters.AddWithValue("Descripcion_in", torneo.Descripcion);
             cmd.Parameters.AddWithValue("IdEquipoUsuario_in", torneo.IdEquipoUsuario);
+            cmd.Parameters.AddWithValue("Id", torneo.IdTorneo);
             cmd.ExecuteNonQuery();
             connection.Close();
             return id;
         }
-        public static int AltaEquipoRival(EquipoRival equipoRival)
+        public static int ActualizarEquipoRival(EquipoRival equipoRival)
         {
             int id = 1;
             connection.Open();
-            string proceso = "AltaEquipoRival";
+            string proceso = "ActualizarEquipoRival";
             MySqlCommand cmd = new MySqlCommand(proceso, connection);
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.Parameters.AddWithValue("NombreEquipo_in", equipoRival.NombreEquipo);
             cmd.Parameters.AddWithValue("ImagenEscudo_in", equipoRival.ImagenEscudo);
             cmd.Parameters.AddWithValue("IdEquipoUsuario_in", equipoRival.IdEquipoUsuario);
+            cmd.Parameters.AddWithValue("Id", equipoRival.IdEquipoRival);
             cmd.ExecuteNonQuery();
             connection.Close();
             return id;
