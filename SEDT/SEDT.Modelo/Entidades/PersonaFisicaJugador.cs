4098443460b252d723b89d836782e7ba5989eda1
leo <leoimoli@gmail.com>
2017-10-30 09:37:45 -0300

    Arreglo clase Entidades

--- SEDT/SEDT.Modelo/Entidades/PersonaFisicaJugador.cs
+++ SEDT/SEDT.Modelo/Entidades/PersonaFisicaJugador.cs
@@ -8,12 +8,15 @@ namespace SEDT.Modelo.Entidades
 {
     public class PersonaFisicaJugador
     {
-        public int IdJugadorCartera { get; set; }
+        public int IdPersonaFisicaJugador { get; set; }
         public int idUsuario { get; set; }
+        public string Dni { get; set; }
         public string Apellido { get; set; }
         public string Nombre { get; set; }
         public string Apodo { get; set; }
-
-     
+        public DateTime FechaNacimiento { get; set; }
+        public string Altura { get; set; }
+        public string Peso { get; set; }
+        public byte[] Imagen { get; set; }
     }
 }
